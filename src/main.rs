4779f48d0c503f229d8754049d2115d41cdc55bb
Mike S <mikestiller@gmail.com>
2024-09-02 18:14:47 -0700

    Refactored code to split into main and library files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {query}");
-    println!("Searching for {file_path}");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
